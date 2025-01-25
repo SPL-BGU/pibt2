62776b53e24d55c6c90927199775f25eb25ba79f
Rotem Lev Lehman <Rotem12430@gmail.com>
2025-01-25 23:53:57 +0200

    bugfix(pibt): field of view is now shared between same agent's mock-agents and looked up using the CounterWithSize

--- pibt2/include/pibt.hpp
+++ pibt2/include/pibt.hpp
@@ -10,6 +10,112 @@
 
 #pragma once
 #include "solver.hpp"
+#include <set>
+
+/**
+ * @brief This class allows to count the number of agent groups that occupy a node.
+ * Also, for each agent group, it allows to count the number of agents from that group that occupy a node.
+ * 
+ * @note It is used for the field of view of agents.
+ */
+class CounterWithSize {
+private:
+  std::vector<size_t> agent_groups;
+  std::set<size_t> agents_occupied;
+  size_t _size;
+  size_t agent_groups_count;
+public:
+  CounterWithSize(size_t _agent_groups_count) : agent_groups(_agent_groups_count, 0), agents_occupied(), _size(0), agent_groups_count(_agent_groups_count) {}
+
+  /**
+   * @brief The size of the counter for all agent groups.
+   * 
+   * @return size_t The size of the counter.
+   */
+  size_t size() const { return _size; }
+
+  /**
+   * @brief Get the agents occupied set.
+   * 
+   * @return std::set<size_t> The agents that see the node (in their field of view).
+   */
+  std::set<size_t> get_agents_occupied() const { return agents_occupied; }
+
+  /**
+   * @brief Get the number of agents in the given agent group that occupy the node.
+   * 
+   * @param i The agent group id.
+   * @return size_t The number of agents in the given agent group that occupy the node.
+   */
+  size_t get_agent_group_count(size_t i) const { 
+    if (i >= agent_groups_count) {
+      throw std::runtime_error("Index out of bounds.");
+    }
+    return agent_groups[i]; 
+  }
+
+  /**
+   * @brief Checks if the field of view is occupied by the given agent.
+   * 
+   * @param agent_id The agent id.
+   * @return true If occupied by the agent.
+   * @return false Otherwise.
+   */
+  bool is_occupied_by_agent(size_t agent_id) const {
+    return agents_occupied.count(agent_id) > 0;
+  }
+
+  /**
+   * @brief Increase the number of agents in the given agent group that occupy the node by one.
+   * 
+   * @param i The agent group id.
+   */
+  void occupy(size_t agent_id, size_t agent_group_id) {
+    if (agent_group_id >= agent_groups_count) {
+      throw std::runtime_error("Index out of bounds.");
+    }
+    if (agents_occupied.count(agent_id) > 0) {
+      throw std::runtime_error("The agent is already occupying the node.");
+    }
+    ++_size;
+    ++agent_groups[agent_group_id];
+    agents_occupied.insert(agent_id);
+  }
+
+  /**
+   * @brief Decrease the number of agents in the given agent group that occupy the node by one.
+   * 
+   * @param i The agent group id.
+   */
+  void deoccupy(size_t agent_id, size_t agent_group_id) {
+    if (agent_group_id >= agent_groups_count) {
+      throw std::runtime_error("Index out of bounds.");
+    }
+    if(agents_occupied.count(agent_id) == 0) {
+      throw std::runtime_error("The agent is not occupying the node.");
+    }
+    if (agent_groups[agent_group_id] == 0) {
+      throw std::runtime_error("Cannot decrease from index " + std::to_string(agent_group_id) + " because it is already 0.");
+    }
+    --_size;
+    --agent_groups[agent_group_id];
+    agents_occupied.erase(agent_id);
+  }
+
+  std::string to_string() const {
+    std::string str = "groups: [";
+    for (size_t i = 0; i < agent_groups.size(); ++i) {
+      str += std::to_string(agent_groups[i]) + ", ";
+    }
+    str += "], agents: [";
+    for (size_t agent_id : agents_occupied) {
+      str += std::to_string(agent_id) + ", ";
+    }
+    str += "]";
+    return str;
+  }
+
+};
 
 class PIBT : public MAPF_Solver
 {
@@ -29,29 +135,75 @@ private:
   };
   using Agents = std::vector<Agent*>;
 
+  using Fields = std::vector<CounterWithSize>;
+
   // <node-id, agent>, whether the node is occupied or not
   // work as reservation table
   Agents occupied_now;
   Agents occupied_next;
 
+  // All agents
+  Agents all_agents;
+
+  // <node-id, set<agent_group_id>>, whether the node is in the field of view of an agent or not.
+  // A node can be in the field of view of several agent groups.
+  // Nodes that are not in the field of view of an agent are empty sets.
+  // Work as reservation table.
+  Fields occupied_field_of_view_now;
+  Fields occupied_field_of_view_next;
+
   // option
   bool disable_dist_init = false;
   int field_of_view_radius = 0; // field_of_view_radius - 
           // each agent must keep away from other agents by at least this radius.
+  size_t k;  // k - amount of mock agents used for each agent.
 
-  // result of priority inheritance: true -> valid, false -> invalid
-  bool funcPIBT(Agent* ai, Agent* aj = nullptr);
+  /**
+   * @brief Actual priority inheritance with backtracking algorithm.
+   * 
+   * @param ai The agent to plan the next location for.
+   * @param aj The agent that has higher priority than ai, and ai needs to replan because of aj. If ai is the highest priority agent, then aj is nullptr.
+   * @return std::set<size_t> The agents that were affected from the planning of the agent ai. If invalid plan for ai, returns an empty set.
+   */
+  std::set<size_t> funcPIBT(Agent* ai, Agent* aj = nullptr);
 
   /**
    * @brief Occupies the entire field of view of the given node to be of the given agent.
    * 
-   * @note In order to un-occupy, just insert nullptr as the given agent.
+   * @note In order to de-occupy, call the deoccupy function.
    * 
    * @param occupied The occupied nodes vector to occupy at.
+   * @param occupied_field_of_view The occupied field of view vector to occupy at.
    * @param node The node to occupy it's field of view.
    * @param agent The agent to occupy inside the field of view.
+   * 
    */
-  void occupy(Agents & occupied, Node * node, Agent * agent);
+  void occupy(Agents & occupied, Fields & occupied_field_of_view, Node * node, Agent * agent);
+
+  /**
+   * @brief De-occupies the entire field of view of the given node to be of the given agent.
+   * 
+   * @param occupied The occupied nodes vector to de-occupy at.
+   * @param occupied_field_of_view The occupied field of view vector to de-occupy at.
+   * @param node The node to de-occupy it's field of view.
+   * @param agent The agent to de-occupy inside the field of view.
+   * 
+   */
+  void deoccupy(Agents & occupied, Fields & occupied_field_of_view, Node * node, Agent * agent);
+
+  /**
+   * @brief Checks if the given agent can occupy the given node.
+   * 
+   * @note A node can be occupied if it is not in the field of view of some other agent's group, and not already occupied.
+   * 
+   * @param occupied The occupied nodes vector to check if the node is occupied.
+   * @param occupied_field_of_view The occupied field of view vector to check if the node is in the field of view of another agent's group.
+   * @param node The node to check if it can be occupied.
+   * @param agent The agent to check if can occupy the node.
+   * @return true If the agent can occupy the node.
+   * @return false Otherwise.
+   */
+  bool can_occupy(Agents & occupied, Fields & occupied_field_of_view, Node * node, Agent * agent);
 
   // main
   void run();
--- pibt2/src/pibt.cpp
+++ pibt2/src/pibt.cpp
@@ -2,18 +2,109 @@
 
 const std::string PIBT::SOLVER_NAME = "PIBT";
 
+
 PIBT::PIBT(MAPF_Instance* _P)
     : MAPF_Solver(_P),
       occupied_now(Agents(G->getNodesSize(), nullptr)),
-      occupied_next(Agents(G->getNodesSize(), nullptr))
+      occupied_next(Agents(G->getNodesSize(), nullptr)),
+      occupied_field_of_view_now(Fields()),
+      occupied_field_of_view_next(Fields())
 {
   solver_name = PIBT::SOLVER_NAME;
 }
 
-void PIBT::occupy(Agents & occupied, Node * node, Agent * agent){
+/**
+ * @brief Calculate the number of agent groups.
+ * 
+ * @param P The problem instance.
+ * @param k The size of each agent group.
+ * @return size_t The number of agent groups.
+ */
+inline size_t get_num_of_agent_groups(MAPF_Instance* P, size_t k)
+{
+  if (P->getNum() % k != 0) {
+    throw std::runtime_error(
+        "The given amount of agents must be divisible by k.\n");
+  }
+  return P->getNum() / k;
+}
+
+/**
+ * @brief Get the agent group id.
+ *
+ * @note The group is determined by the agent_id divided by k.
+ *
+ * @param agent_id The mock agent id.
+ * @param k The amount of mock agents used for each agent.
+ * @return size_t The group id.
+ */
+inline size_t get_agent_group_id(int agent_id, size_t k) { return agent_id / k; }
+
+bool PIBT::can_occupy(Agents& occupied, Fields& occupied_field_of_view,
+                      Node* node, Agent* agent)
+{
+  if (nullptr == agent) {
+    throw std::runtime_error("Agent must not be nullptr.\n");
+  }
+  // Check if node already occupied
+  if (occupied[node->id] != nullptr) {
+    return false;
+  }
+
+  size_t group_id = get_agent_group_id(agent->id, k);
+
+  // Check if node is in the field of view of another agent's group
+  size_t other_groups_amount = occupied_field_of_view[node->id].size() -
+                            occupied_field_of_view[node->id].get_agent_group_count(group_id);
+  return other_groups_amount == 0;
+}
+
+void PIBT::occupy(Agents& occupied, Fields& occupied_field_of_view, Node* node,
+                  Agent* agent)
+{
+  if (nullptr == agent) {
+    throw std::runtime_error("Agent must not be nullptr.\n");
+  }
+  if (occupied[node->id] != nullptr) {
+    throw std::runtime_error("The node is already occupied.\n");
+  }
+  if (!can_occupy(occupied, occupied_field_of_view, node, agent)) {
+    throw std::runtime_error("The node cannot be occupied.\n");
+  }
+
   Nodes field_of_view = get_field_of_view(G, node, P->getFieldOfViewRadius());
-  for(const Node * current_node : field_of_view){
-    occupied[current_node->id] = agent;
+  size_t group_id = get_agent_group_id(agent->id, k);
+
+  for (const Node* current_node : field_of_view) {
+    occupied_field_of_view[current_node->id].occupy(agent->id, group_id);
+  }
+  occupied[node->id] = agent;
+}
+
+void PIBT::deoccupy(Agents& occupied, Fields& occupied_field_of_view,
+                    Node* node, Agent* agent)
+{
+  if (nullptr == agent) {
+    throw std::runtime_error("Agent must not be nullptr.\n");
+  }
+  if (occupied[node->id] != agent) {
+    throw std::runtime_error("The node is not occupied by the given agent.\n");
+  }
+
+  Nodes field_of_view = get_field_of_view(G, node, P->getFieldOfViewRadius());
+  size_t group_id = get_agent_group_id(agent->id, k);
+
+  for (const Node* current_node : field_of_view) {
+    if (occupied_field_of_view[current_node->id].is_occupied_by_agent(agent->id)) {
+      occupied_field_of_view[current_node->id].deoccupy(agent->id, group_id);
+    } else {
+      throw std::runtime_error(
+          "The node is not occupied by the given agent.\n");
+    }
+  }
+  if(occupied[node->id] == agent){
+    // Checking this since the force flag could be set to true
+    occupied[node->id] = nullptr;
   }
 }
 
@@ -43,7 +134,12 @@ void PIBT::run()
                          d,                          // dist from s -> g
                          getRandomFloat(0, 1, MT)};  // tie-breaker
     A.push_back(a);
-    occupy(occupied_now, s, a);
+    all_agents.push_back(a);
+    if (can_occupy(occupied_now, occupied_field_of_view_now, s, a)) {
+      occupy(occupied_now, occupied_field_of_view_now, s, a);
+    } else {
+      throw std::runtime_error("Failed in initialization - the node (" + std::to_string(s->pos.x) + "," + std::to_string(s->pos.y) + ") cannot be occupied by agent " + std::to_string(i));
+    }
   }
   solution.add(P->getConfigStart());
 
@@ -58,22 +154,58 @@ void PIBT::run()
       // if the agent has next location, then skip
       if (a->v_next == nullptr) {
         // determine its next location
-        funcPIBT(a);
+        std::set<size_t> affected_agents = funcPIBT(a);
+        if(affected_agents.empty()){
+          if (a->v_next != nullptr){
+            throw std::runtime_error("The agent " + std::to_string(a->id) + " has a next location, but the affected agents are empty.");
+          }
+          if (!can_occupy(occupied_next, occupied_field_of_view_next, a->v_now, a)){
+            std::cout << "The agent " << a->id << " cannot occupy the node [id=" << a->v_now->id << "] (" << a->v_now->pos.x << "," << a->v_now->pos.y << ")" << std::endl;
+            std::cout << "Current node field of view is: " << occupied_field_of_view_next[a->v_now->id].to_string() << std::endl;
+            std::cout << "Now printing the entire occupied vector:" << std::endl;
+            std::cout << "occupied_now: {";
+            for (size_t i = 0; i < P->getNum(); i++){
+              Agent * agent = A[i];
+              std::cout << "agents[" << i << "].v_now = (" << agent->v_now->pos.x << "," << agent->v_now->pos.y << "), ";
+            }
+            std::cout << "}" << std::endl;
+            std::cout << "occupied_next: {";
+            for (size_t i = 0; i < P->getNum(); i++){
+              Agent * agent = A[i];
+              if (agent->v_next != nullptr){
+                std::cout << "agents[" << i << "].v_next = (" << agent->v_next->pos.x << "," << agent->v_next->pos.y << "), ";
+              }
+              else{
+                std::cout << "agents[" << i << "].v_next = nullptr, ";
+              }
+            }
+            std::cout << "}" << std::endl;
+            exit(1);
+          }
+          // Failed to plan the next location for agent a, keep it in it's current location
+          occupy(occupied_next, occupied_field_of_view_next, a->v_now, a);
+          a->v_next = a->v_now;
+        }
       }
     }
 
     // acting
     bool check_goal_cond = true;
     Config config(P->getNum(), nullptr);
+    // we split the deoccupy and occupy steps to avoid conflicts in the occupy step
+    // deoccupy all agents
     for (auto a : A) {
-      // clear
-      if (occupied_now[a->v_now->id] == a) {
-        occupy(occupied_now, a->v_now, nullptr);
-      }
-      occupy(occupied_next, a->v_next, nullptr);
+      deoccupy(occupied_now, occupied_field_of_view_now, a->v_now, a);
+      deoccupy(occupied_next, occupied_field_of_view_next, a->v_next, a);
+    }
+    // occupy next location to current location
+    for (auto a : A) {
       // set next location
       config[a->id] = a->v_next;
-      occupy(occupied_now, a->v_next, a);
+      if (!can_occupy(occupied_now, occupied_field_of_view_now, a->v_next, a)) {
+        throw std::runtime_error("The agent " + std::to_string(a->id) + " cannot occupy the node [id=" + std::to_string(a->v_next->id) + "] (" + std::to_string(a->v_next->pos.x) + "," + std::to_string(a->v_next->pos.y) + ")");
+      }
+      occupy(occupied_now, occupied_field_of_view_now, a->v_next, a);
       // check goal condition
       check_goal_cond &= (a->v_next == a->g);
       // update priority
@@ -104,7 +236,7 @@ void PIBT::run()
   for (auto a : A) delete a;
 }
 
-bool PIBT::funcPIBT(Agent* ai, Agent* aj)
+std::set<size_t> PIBT::funcPIBT(Agent* ai, Agent* aj)
 {
   // compare two nodes
   auto compare = [&](Node* const v, Node* const u) {
@@ -128,44 +260,72 @@ bool PIBT::funcPIBT(Agent* ai, Agent* aj)
   std::sort(C.begin(), C.end(), compare);
 
   for (auto u : C) {
+    std::set<size_t> affected_agents;
+    bool fallback = false;
     // avoid conflicts
-    if (occupied_next[u->id] != nullptr) continue;
-    if (aj != nullptr && u == aj->v_now) continue;
+    if (occupied_next[u->id] != nullptr) continue; // TODO - check if needed or included in can_occupy
+    if (aj != nullptr && u == aj->v_now) continue; // Needed only when field of view is 0
+    // avoid field of view conflicts
+    if (!can_occupy(occupied_next, occupied_field_of_view_next, u, ai)) continue;
 
     // reserve
-    occupy(occupied_next, u, ai);
+    occupy(occupied_next, occupied_field_of_view_next, u, ai);
 
     ai->v_next = u;
 
-    auto ak = occupied_now[u->id];
-    if (ak != nullptr && ak->v_next == nullptr) {
-      if (!funcPIBT(ak, ai)) {
-        //remove occupation for the next iterations:
-        occupy(occupied_next, u, nullptr);
-        continue;  // replanning
+    // Move all agents that are in the field of view of the agent in their current state.
+    // This includes the agent (if exists) that is currently at u. (This can happen only when field of view is 0).
+    std::set<size_t> agents_viewing_u_now = occupied_field_of_view_now[u->id].get_agents_occupied();
+    for (size_t agent_id : agents_viewing_u_now) {
+      if (ai->id == agent_id) {
+        continue;
+      }
+
+      Agent* ak = all_agents[agent_id];
+      if (ak->v_next == nullptr) {
+        std::set<size_t> affected_agents_k = funcPIBT(ak, ai);
+        if (affected_agents_k.empty()) {
+          fallback = true;
+          // remove occupation for the next iterations:
+          deoccupy(occupied_next, occupied_field_of_view_next, u, ai);
+          ai->v_next = nullptr;
+          break;  // need to fallback
+        }
+        else{
+          // Union the sets:
+          affected_agents.insert(affected_agents_k.begin(), affected_agents_k.end());
+        }
       }
     }
+    if (fallback) {
+      for (size_t agent_id : affected_agents) {
+        Agent* ak = all_agents[agent_id];
+        deoccupy(occupied_next, occupied_field_of_view_next, ak->v_next, ak);
+        ak->v_next = nullptr;
+      }
+      continue;
+    }
     // success to plan next one step
-    return true;
+    affected_agents.insert(ai->id);
+    return affected_agents;
   }
-
-  // failed to secure node
-  occupy(occupied_next, ai->v_now, ai);
-  ai->v_next = ai->v_now;
-  return false;
+  // Failed to plan the next location for agent a_i, return empty set
+  return std::set<size_t>();
 }
 
 void PIBT::setParams(int argc, char* argv[])
 {
   bool field_of_view_radius_provided = false;
+  bool k_provided = false;
   struct option longopts[] = {
       {"disable-dist-init", no_argument, 0, 'd'},
       {"field-of-view-radius", required_argument, nullptr, 'r'},
+      {"mock-agents-num", required_argument, nullptr, 'k'},
       {0, 0, 0, 0},
   };
   optind = 1;  // reset
   int opt, longindex;
-  while ((opt = getopt_long(argc, argv, "dr:", longopts, &longindex)) != -1) {
+  while ((opt = getopt_long(argc, argv, "dr:k:", longopts, &longindex)) != -1) {
     switch (opt) {
       case 'd':
         disable_dist_init = true;
@@ -174,13 +334,35 @@ void PIBT::setParams(int argc, char* argv[])
         field_of_view_radius_provided = true;
         field_of_view_radius = std::stoi(optarg);
         break;
+      case 'k':
+        k = std::stoi(optarg);
+        k_provided = true;
+        break;
       default:
         break;
     }
   }
   if (!field_of_view_radius_provided) {
-    std::cerr << "Error: The -r (or --field-of-view-radius) option is required." << std::endl;
+    std::cerr << "Error: The -r (or --field-of-view-radius) option is required."
+              << std::endl;
     exit(1);
+  }
+  if (!k_provided) {
+    std::cerr << "Error: The -k (or --mock-agents-num) option is required."
+              << std::endl;
+    exit(1);
+  }
+  if (k == 0) {
+    std::cerr << "Error: The -k (or --mock-agents-num) option must be bigger "
+                 "or equal to 1."
+              << std::endl;
+    exit(1);
+  }
+  // initialize the counters with the number of agent groups
+  size_t count = get_num_of_agent_groups(P, k);
+  for(int i = 0; i < G->getNodesSize(); ++i) {
+    occupied_field_of_view_now.push_back(CounterWithSize(count));
+    occupied_field_of_view_next.push_back(CounterWithSize(count));
   }
 }
 
@@ -194,5 +376,9 @@ void PIBT::printHelp()
             << "  -r --field-of-view-radius"
             << "     "
             << "radius that other agents may see each other\n"
+            << "  -k --mock-agents-num"
+            << "          "
+            << "number of mock agents to use. This is used to group mock "
+               "agents together so they can be in the same field of view\n"
             << std::endl;
 }
